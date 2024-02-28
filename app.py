3bd746e17d2563d43c19c93a09d780a477142cb4
Michelle <michellejslei@gmail.com>
2024-02-27 17:44:41 -0800

    calendar events added

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, request, send_file, redirect, url_for, send_from_directory, flash
+from flask import Flask, render_template, request, redirect, flash, jsonify, url_for
 import os
 import PyPDF2
 import re
 from dateutil.parser import parse
+from datetime import datetime
 
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'pdf'} 
+
+events_storage = []
 
 # Create the Flask application
 app = Flask(__name__)
@@ -14,6 +17,7 @@ app.secret_key = 'your_secret_key'
 
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
+@app.route('/')
 def index():
     return render_template('index.html')
 
@@ -59,26 +63,47 @@ def find_dates_and_assignments(text):
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
+    global events_storage
     if request.method == 'POST':
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
+        
         file = request.files['file']
+
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
+        
         if file and allowed_file(file.filename):
             filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
             file.save(filename)
 
             extracted_text = extract_text_from_pdf(filename)
             extracted_data = find_dates_and_assignments(extracted_text)
-            # print(extracted_data)
             
+            # Clear previous events and store new ones
+            events_storage.clear()
+            for date, detail in extracted_data:
+                events_storage.append({'assignment': detail, 'date': date})
+                
             return render_template('index.html', extracted_data=extracted_data)
+            # return redirect(url_for('calendar_events'))
     
-    # Render the template without extracted text if GET request or no file uploaded
-    return render_template('index.html', extracted_text=None)
+    return render_template('index.html')
+
+@app.route('/calendar_events', methods=['GET'])
+def calendar_events():
+    def get_your_events():
+        # Return the global list of events
+        return events_storage
+
+    events = get_your_events()
+    fullcalendar_events = [
+        {'title': event['assignment'], 'start': event['date'].isoformat()}
+        for event in events
+    ]
+    return jsonify(fullcalendar_events)
 
 if __name__ == '__main__':
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)  # Create upload folder if it doesn't exist
